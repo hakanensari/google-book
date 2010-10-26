cb23d58af69e935408040804714d68e4439e2c4d
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-10-26 13:37:21 +0100

    create docs

---
+++ panel/tree.js
@@ -0,0 +1,1 @@
+var tree = [["","","files",[["LICENSE","files/LICENSE.html","",[]],["README.md","files/README_md.html","",[]],["","","lib",[["","","googlebook",[["finder.rb","files/lib/googlebook/finder_rb.html","",[]],["version.rb","files/lib/googlebook/version_rb.html","",[]]]],["googlebook.rb","files/lib/googlebook_rb.html","",[]]]]]],["GoogleBook","classes/GoogleBook.html"," < Object",[]]]
